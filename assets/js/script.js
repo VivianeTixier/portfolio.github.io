5ba58dbf11b49cf1ad901ddb00df2a8d756f0a42
Viviane <viviane.tixier.simplon@gmail.com>
2019-09-03 11:34:44 +0200

    ajustement du côté responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,28 +2,8 @@
 $(document).ready(function () {
     $('.center').slick({
         centerMode: true, //pour avoir l'image ciblée au milieu
-        centerPadding: '60px',
+        centerPadding: '20wv',
         slidesToShow: 3,
-        responsive: [
-            {
-                breakpoint: 768,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 3
-                }
-            },
-            {
-                breakpoint: 480,
-                settings: {
-                    arrows: false,
-                    centerMode: true,
-                    centerPadding: '40px',
-                    slidesToShow: 2
-                }
-            }
-        ]
     });
 });
 
